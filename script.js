c4bb444f99ed19f922ba9cb947e27f922228008f
Himanshu Singh <himanshusingh1k@gmail.com>
2026-02-08 12:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function calculateArea() {
   let radius = prompt("Enter the radius of the circle:");
   let ans = 3.14 * radius ** 2;
-  alert(` The area of the circle with radius 4 is ${ans}`);
+  alert(`The area of the circle with radius ${radius} is ${ans}`);
 }
 
 calculateArea();
